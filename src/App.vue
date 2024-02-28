5f7cf462d86289b2a7ef867c1f261872f0637eb1
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-02-28 18:23:11 +0500

    used a simple layout from vuetify for todo app

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+  <v-list-item title="My Application" subtitle="Vuetify">Vuetify Todo</v-list-item>
+  <v-divider></v-divider>
+  <v-list-item link title="List Item 1" :to="homePage.to">Todo</v-list-item>
+  <v-list-item link title="List Item 2" :to="aboutPage.to">About</v-list-item>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const drawer = ref(null)
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      // items: [
+      //   {title: 'Todo', to: '/'},
+      //   {title: 'About', to: 'about'}
+      //   ]
+      homePage: {to: '/'},
+      aboutPage: {to: '/about'}
+     }),
+  }
+</script>
