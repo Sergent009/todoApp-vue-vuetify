768d2a383b61bc67190281a5bc5a848a9b620e44
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-03-01 16:12:21 +0500

    A simple todo app is completed in vue js using vuetify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home pa-6">
+    <v-text-field outlined label="Add Task" append-icon="mdi-plus" hide-details clearable v-model="newTaskTitle" @click:append="addTask" @keyup.enter="addTask">
+    </v-text-field>
   <div v-for="task in tasks" :key="task.id">
     <!-- p for padding, t for top, 0 is the size  -->
     <v-list flat class="pt-0">
-      <v-list-item @click="doneTask(task.id)">
+      <v-list-item @click="doneTask(task.id)" :class="{'blue lighten-5': task.done}">
         <template v-slot:default>
 
           <v-list-item-action>
@@ -11,8 +13,14 @@
           </v-list-item-action>
 
           <v-list-item-content>
-            <v-list-item-title>{{task.title}}</v-list-item-title>
+            <v-list-item-title :class="{'text-decoration-line-through' : task.done}">{{task.title}}</v-list-item-title>
           </v-list-item-content>
+        
+          <v-list-item-action>
+            <v-btn @click.stop="deleteTask(task.id)" icon>
+              <v-icon color="primary lighten-1">mdi-delete</v-icon>
+            </v-btn>
+          </v-list-item-action>
 
         </template>
       </v-list-item>
@@ -28,31 +36,29 @@ export default {
   name: 'Home',
   data(){
     return{
-      tasks: [
-        {
-          id: 1,
-          title: 'Wake Up',
-          done: false
-        },
-        {
-          id: 2,
-          title: 'Do some good deeds for Akhirah',
-          done: false
-        },
-        {
-          id: 3,
-          title: 'Do some coding to earn money',
-          done: false
-        },
-      ]
+      newTaskTitle: '',
+      tasks: []
     }
   },
 
   methods: {
     doneTask(id){
       let task = this.tasks.filter(task => task.id === id)[0]
-        task.done = !task.done
-  }
+      task.done = !task.done
+    },
+    deleteTask(id){
+      this.tasks = this.tasks.filter(task => task.id !== id)
+    },
+
+    addTask(){
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTitle = ''
+    }
 }
 }
 </script>
